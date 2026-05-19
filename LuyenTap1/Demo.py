26f070a0e8de016f79fe124d8e0af741aa433981
ntrituee <n.tri.tuee@gmail.com>
2026-05-20 00:37:35 +0700

    Add files via upload

---
+++ LuyenTap1/Demo.py
@@ -0,0 +1,22 @@
+#Bài 1
+#a: số nguyên
+a=6
+#b: số thực
+b=1.6
+#c: chuỗi
+c="hello"
+print(a)
+print(b)
+print(c)
+#Bài 2
+r=5
+Pi=3.14
+C=2*r*Pi
+print(C)
+#Bài 3
+a=int(input("số thứ nhất"))
+b=int(input("số thưs hai"))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
