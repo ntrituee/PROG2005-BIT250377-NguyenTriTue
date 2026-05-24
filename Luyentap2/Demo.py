013c849443fa74541b3f289845042c68d36e329e
ntrituee <n.tri.tuee@gmail.com>
2026-05-25 00:24:36 +0700

    Add files via upload

---
+++ Luyentap2/Demo.py
@@ -0,0 +1,11 @@
+#Bài 2
+a=float(input("số thứ nhất"))
+b=float(input("số thứ hai"))
+c=float(input("số thứ ba"))
+max_num=max(a,b,c)
+print("số lớn nhất",max_num)
+#Bài 6
+C=float(input("Nhiệt độ(°C)"))
+F=C*9/5+32
+print("Nhiệt độ(°F)",F)
+
